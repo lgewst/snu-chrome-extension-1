b44d48e629eccc65f05d83c0757a7d7bc44d9876
aeri206 <aeri33590@gmail.com>
2018-10-16 21:22:22 +0900

    modified eslint setting comma-dangle

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "quotes": ["error", "double"],
-        "indent": ["error", 4]
+        "indent": ["error", 4],
+        "comma-dangle": ["error", "never"]
     },
     "env": {
         "webextensions": true
