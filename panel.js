07552969246982ac66d5fab1003085ae646d4e8e
sunnyday0208 <sunnyday0208@naver.com>
2018-11-22 14:44:52 +0900

    The problem of printing [object Object] is solved

--- panel.js
+++ panel.js
@@ -1,18 +1,18 @@
 chrome.tabs.onActivated.addListener(function(tabId, changeInfo, tab) {
    
-    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.isContainer(document.activeElement);",{useContentScriptContext : true},function(result) {
+    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.isContainer(document.activeElement);", {useContentScriptContext : true}, function(result) {
         document.getElementById('container').innerHTML = result;
     });
-    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'up');",{useContentScriptContext : true},function(result) {
-        document.getElementById('up').innerHTML = result;
+    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'up').outerHTML;", {useContentScriptContext : true}, function(result) {
+        document.getElementById('up').innerHTML = "<xmp>" + result + "</xmp>";
     });
-    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'down');",{useContentScriptContext : true},function(result) {
-        document.getElementById('down').innerHTML = result;
+    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'down').outerHTML;", {useContentScriptContext : true}, function(result) {
+        document.getElementById('down').innerHTML = "<xmp>" + result + "</xmp>";
     });
-    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'left');",{useContentScriptContext : true},function(result) {
-        document.getElementById('left').innerHTML = result;
+    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'left').outerHTML;", {useContentScriptContext : true}, function(result) {
+        document.getElementById('left').innerHTML = "<xmp>" + result + "</xmp>";
     });
-    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'right');",{useContentScriptContext : true},function(result) {
-        document.getElementById('right').innerHTML = result;
+    chrome.devtools.inspectedWindow.eval("__spatialNavigation__.findNextTarget(document.activeElement, 'right').outerHTML;", {useContentScriptContext : true}, function(result) {
+        document.getElementById('right').innerHTML = "<xmp>" + result + "</xmp>";
     });
  }); 
