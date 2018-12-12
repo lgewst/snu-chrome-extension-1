4c0e1fa4eb76b505662a22d8bd249ad74031ff61
aeri206 <aeri33590@gmail.com>
2018-12-12 19:22:05 +0900

    overlay all possible candidate list of 4direction (checkbox, basic)

--- panel.js
+++ panel.js
@@ -1,3 +1,4 @@
+
 var backgroundPageConnection = chrome.runtime.connect({
     name: "panel"
 });
@@ -7,7 +8,40 @@ backgroundPageConnection.postMessage({
     tabId: chrome.devtools.inspectedWindow.tabId
 });
 
+
+// add basic coloring & decoloring function
+function coloring(dir) {   
+    const pre_color = 'function find(){ var tmp = __spatialNavigation__.findCandidates(document.activeElement, "';
+    chrome.tabs.executeScript({
+        code: pre_color.concat(dir, '"); var i; for (i = 0 ; i < tmp.length ; i++){ tmp[i].style.outline = "solid #B0C4DE"; } } find();')
+    });
+}
+
+function decoloring(dir){
+
+    const pre_decolor = 'function find(){ var tmp = __spatialNavigation__.findCandidates(document.activeElement, "';
+    chrome.tabs.executeScript({
+        code: pre_decolor.concat(dir,'"); var i; for (i = 0 ; i < tmp.length ; i++){ tmp[i].style.outline = "transparent"; } } find();')
+    });
+}
+
 var direction = ["up","down","left","right"];
+
+for (var idx = 0 ; idx < direction.length ; idx++){
+    // set onclick methond on each direction button
+    try {throw direction[idx]}
+    catch (way) {
+        document.getElementById("Button_".concat(way)).onclick = function(){
+            if (this.checked){
+                coloring(way);
+            }
+            else {
+                decoloring(way);
+            }
+        }
+    }
+}
+
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
    
@@ -15,20 +49,18 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         document.getElementById('container').innerText = result;
     });
 
-
-
     chrome.devtools.inspectedWindow.eval("document.activeElement.focusableAreas({'mode': 'visible'}).map(a => a.outerHTML);", {useContentScriptContext : true}, function(result) {
         if (result.length == 0){
             result = "None";
         }
-        document.getElementById('visible').innerText = result.replace(/(\r\n\t|\n|\r\t)/gm,"");
+        document.getElementById('visible').innerText = result.toString().replace(/(\r\n\t|\n|\r\t)/gm,"");
     });
 
     chrome.devtools.inspectedWindow.eval("document.activeElement.focusableAreas({'mode': 'all'}).map(a => a.outerHTML);", {useContentScriptContext : true}, function(result) {
         if (result.length == 0){
             result = "None";
         }
-        document.getElementById('all').innerText = result.replace(/(\r\n\t|\n|\r\t)/gm,"");
+        document.getElementById('all').innerText = result.toString().replace(/(\r\n\t|\n|\r\t)/gm,"");
     });
 
 
@@ -41,8 +73,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             chrome.devtools.inspectedWindow.eval(cmd_0, {useContentScriptContext : true}, function(result) {
                 try {throw result}
                 catch(res){
-                    if (res == null) document.getElementById(way).innerText = null;
-                    else document.getElementById(way).innerText = res.replace(/(\r\n\t|\n|\r\t)/gm,"");    
+                    if (res === undefined) document.getElementById(way).innerText = undefined;
+                    else document.getElementById(way).innerText = res.toString().replace(/(\r\n\t|\n|\r\t)/gm,"");    
                 }
                 document.getElementById(way).setAttribute('cmd','next');
             });
@@ -55,10 +87,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 document.getElementById(search_id).setAttribute('cmd','spatnav_search');
                 try {throw result2}
                 catch(res2){
-                    if (res2 == null)
-                        document.getElementById(search_id).innerText = null;
+                    if (res2 === undefined)
+                        document.getElementById(search_id).innerText = undefined;
                     else
-                        document.getElementById(search_id).innerText = res2.replace(/(\r\n\t|\n|\r\t)/gm,"");
+                        document.getElementById(search_id).innerText = res2.toString().replace(/(\r\n\t|\n|\r\t)/gm,"");
                 }
             });
         })();       
@@ -66,51 +98,77 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 
     chrome.devtools.inspectedWindow.eval("function candidates_up(){ var temp = __spatialNavigation__.findCandidates(document.activeElement, 'up'); var distance = []; var i; var dis_candidate = []; for(i = 0; i < temp.length; i++){ distance[i] = __spatialNavigation__.getDistanceFromTarget(document.activeElement, temp[i], 'up'); dis_candidate[i] = [temp[i].outerHTML, distance[i]];} return dis_candidate;} candidates_up();", {useContentScriptContext : true}, function(result) {
+        if (result === undefined){
+            document.getElementById('candidates_up').innerText = undefined;
+        }
+        else{
         var i;
         var temp = "";
         for(i = 0; i < result.length; i++){
             temp += (i + 1) + ": " + result[i][0].replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n" + result[i][1] + "\n";
         }
         document.getElementById('candidates_up').innerText = temp;
+    }
     });
 
     chrome.devtools.inspectedWindow.eval("function candidates_down(){ var temp = __spatialNavigation__.findCandidates(document.activeElement, 'down'); var distance = []; var i; var dis_candidate = []; for(i = 0; i < temp.length; i++){ distance[i] = __spatialNavigation__.getDistanceFromTarget(document.activeElement, temp[i], 'down'); dis_candidate[i] = [temp[i].outerHTML, distance[i]];} return dis_candidate;} candidates_down();", {useContentScriptContext : true}, function(result) {
+        if (result === undefined){
+            document.getElementById('candidates_down').innerText = undefined;
+        }
+        else{
         var i;
         var temp = "";
         for(i = 0; i < result.length; i++){
-            temp += (i + 1) + ": " + result[i][0].replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n" + result[i][1] + "\n";
+            temp += (i + 1) + ": " + result[i][0].toString().replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n" + result[i][1] + "\n";
         }
         document.getElementById('candidates_down').innerText = temp;
+    }
     });
 
     chrome.devtools.inspectedWindow.eval("function candidates_left(){ var temp = __spatialNavigation__.findCandidates(document.activeElement, 'left'); var distance = []; var i; var dis_candidate = []; for(i = 0; i < temp.length; i++){ distance[i] = __spatialNavigation__.getDistanceFromTarget(document.activeElement, temp[i], 'left'); dis_candidate[i] = [temp[i].outerHTML, distance[i]];} return dis_candidate;} candidates_left();", {useContentScriptContext : true}, function(result) {
+        if (result === undefined){
+            document.getElementById('candidates_left').innerText = undefined;
+        }
+        else{
         var i;
         var temp = "";
         for(i = 0; i < result.length; i++){
-            temp += (i + 1) + ": " + result[i][0].replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n" + result[i][1] + "\n";
+            temp += (i + 1) + ": " + result[i][0].toString().replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n" + result[i][1] + "\n";
         }
         document.getElementById('candidates_left').innerText = temp;
+    }
     });
 
     chrome.devtools.inspectedWindow.eval("function candidates_right(){ var temp = __spatialNavigation__.findCandidates(document.activeElement, 'right'); var distance = []; var i; var dis_candidate = []; for(i = 0; i < temp.length; i++){ distance[i] = __spatialNavigation__.getDistanceFromTarget(document.activeElement, temp[i], 'right'); dis_candidate[i] = [temp[i].outerHTML, distance[i]];} return dis_candidate;} candidates_right();", {useContentScriptContext : true}, function(result) {
+        if (result === undefined){
+            document.getElementById('candidates_right').innerText = undefined;   
+        }
+        else{
         var i;
         var temp = "";
         for(i = 0; i < result.length; i++){
-            temp += (i + 1) + ": " + result[i][0].replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n" + result[i][1] + "\n";
+            temp += (i + 1) + ": " + result[i][0].toString().replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n" + result[i][1] + "\n";
         }
         document.getElementById('candidates_right').innerText = temp;
+    }
     });
 
 
 
     chrome.devtools.inspectedWindow.eval("function container_list(){ var temp = document.activeElement.getSpatialNavigationContainer(); var list = []; var i = 0; while( temp != null){ list[i] = temp; i = i + 1; temp = temp.getSpatialNavigationContainer();} return list.map(a=>a.outerHTML);} container_list();", {useContentScriptContext : true}, function(result) {
+        if (result === undefined){
+            document.getElementById('container_list').innerText = undefined;
+        }
+        else{
         var i;
         var temp = "";
         for(i = 0; i < result.length; i++){
-            temp += (i + 1) + ": " + result[i].replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n";
+            temp += (i + 1) + ": " + result[i].toString().replace(/(\r\n\t|\n|\r\t)/gm,"") + "\n";
         }
         document.getElementById('container_list').innerText = temp;
+    }
     });    
+    
  }); 
 
  /**
@@ -144,7 +202,7 @@ if (document.getElementById(way).getAttribute('cmd') == 'next') {
     // type 4 : next target
     const pre_out_next = 'window.__spatialNavigation__.findNextTarget(document.activeElement, "';
     chrome.tabs.executeScript({
-        code: pre_out_next.concat(way, '").style.backgroundColor = "transparent"')
+        code: pre_out_next.concat(way, '").style.backgroundolor = "transparent"')
     });
     chrome.tabs.executeScript({
         code: pre_out_next.concat(way, '").style.outline = "transparent"')
@@ -193,4 +251,3 @@ else {
 }
 }
 
- 
